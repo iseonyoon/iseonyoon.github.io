5a50c77348ee75c6d5586e995b9fb61b152cb1e4
daanpaat <iseonyoon@gmail.com>
2024-01-08 08:59:32 +0900

    단팥

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={Monitor} className="Monitor" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
       </header>
+      <body></body>
     </div>
   );
 }
