f26d0e1b0d534d37e33913a46eac0c1ffc084150
iseonyoon <iseonyoon@gmail.com>
2024-11-09 14:50:14 +0900

    s

--- src/riv.js
+++ src/riv.js
@@ -1,7 +1,7 @@
 import rive from "rive-js";
 
 const r = new rive.Rive({
-  src: "https://cdn.rive.app/animations/vehicles.riv",
+  src: "src/riv/mcsplash.riv",
   // OR the path to a discoverable and public Rive asset
   // src: '/public/example.riv',
   canvas: document.getElementById("canvas"),
