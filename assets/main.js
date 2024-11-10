5abcc3d70d2b90d9cd764f1a33718c6befd92f34
iseonyoon <iseonyoon@gmail.com>
2024-11-10 09:07:02 +0900

    20241110

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,6 @@ let canvas;
 let ctx;
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
-canvas.width = 890;
-canvas.height = 550;
+canvas.width = 300;
+canvas.height = 300;
 document.body.appendChild(canvas);
