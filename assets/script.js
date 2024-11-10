7d69ec4b420d778800c9ee0e205d32c0e604741f
iseonyoon <iseonyoon@gmail.com>
2024-11-10 22:37:16 +0900

    20241110

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 let fortunesAvailable = 3;
 
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM content loaded");
   document
     .getElementById("fortune-button")
     .addEventListener("click", getFortune);
@@ -10,23 +11,32 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function getFortune() {
+  console.log("getFortune function called");
   if (fortunesAvailable > 0) {
-    const response = await fetch("fortunes.json");
-    const fortunes = await response.json();
-    const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    try {
+      const response = await fetch("fortunes.json");
+      if (response.ok) {
+        const fortunes = await response.json();
+        const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 
-    document.getElementById("fortune-message").innerHTML = `
-            <p>${fortune.message}</p>
-            <p>이모티콘: ${fortune.emoji}</p>
-            <p>주문: ${fortune.charm}</p>
-            <p>비운의 아이템: ${fortune.unluckyItem}</p>
-        `;
+        document.getElementById("fortune-message").innerHTML = `
+                    <p>${fortune.message}</p>
+                    <p>이모티콘: ${fortune.emoji}</p>
+                    <p>주문: ${fortune.charm}</p>
+                    <p>비운의 아이템: ${fortune.unluckyItem}</p>
+                `;
 
-    fortunesAvailable--;
-    if (fortunesAvailable === 0) {
-      document.getElementById("fortune-button").disabled = true;
+        fortunesAvailable--;
+        if (fortunesAvailable === 0) {
+          document.getElementById("fortune-button").disabled = true;
+        }
+        document.getElementById("share-button").style.display = "block";
+      } else {
+        console.error("포춘 데이터를 불러오는 데 실패했습니다.");
+      }
+    } catch (error) {
+      console.error("Fetch 요청 중 오류가 발생했습니다:", error);
     }
-    document.getElementById("share-button").style.display = "block";
   } else {
     document.getElementById("fortune-message").innerText =
       "오늘의 운세를 모두 사용했습니다. 공유 버튼을 눌러 운세를 더 확인해보세요!";
@@ -34,6 +44,7 @@ async function getFortune() {
 }
 
 function shareFortune() {
+  console.log("shareFortune function called");
   fortunesAvailable++;
   document.getElementById("fortune-button").disabled = false;
   document.getElementById("share-button").style.display = "none";
